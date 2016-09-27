85d88cbc8185260105d5445e05c38f99937133c4
shobanamurugesab <shobana127@gmail.com>
2016-09-27 16:13:27 +0530

    27/9/2016

---
+++ urwardrobebackend/src/main/java/com/urwardrobe/confiig/ApplicationContextConfig.java
@@ -0,0 +1,10 @@
+package com.urwardrobe.confiig;
+import org.springframework.context.annotation;
+public class ApplicationContextConfig {
+@Configuration 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
