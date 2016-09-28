493b4f6f73670b6ae22ec8cd8cea857d31f69790
shobanam95 <shobana127@gmail.com>
2016-09-28 16:07:53 +0530

    Add files via upload

--- urwardrobebackend/src/main/java/com/urwardrobe/confiig/ApplicationContextConfig.java
+++ urwardrobebackend/src/main/java/com/urwardrobe/confiig/ApplicationContextConfig.java
@@ -1,10 +1,68 @@
 package com.urwardrobe.confiig;
-import org.springframework.context.annotation;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(" ")//controller
+
+
+
 public class ApplicationContextConfig {
-@Configuration 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
-	}
-
+@Bean(name = "viewResolver")
+public InternalResourceViewResolver getViewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setPrefix("/WEB-INF/views/");
+    viewResolver.setSuffix(".jsp");
+    return viewResolver;
 }
+
+
+
+@Bean(name = "dataSource")
+public DataSource getDataSource() {
+    BasicDataSource dataSource = new BasicDataSource();
+    dataSource.setDriverClassName("org.h2.Driver");
+    dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+    dataSource.setUsername("sa");
+    dataSource.setPassword("sa");
+ 
+    return dataSource;
+}
+
+private Properties getHibernateProperties() {
+    Properties properties = new Properties();
+    properties.put("hibernate.show_sql", "true");
+      properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+     properties.put("hibernate.hbm2ddl.auto", "create");
+    return properties;
+    }
+    
+@Autowired
+@Bean(name = "sessionFactory")
+public SessionFactory getSessionFactory(DataSource dataSource) {
+localSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+ sessionBuilder.addAnnotatedClasses(User.class); 
+ return sessionBuilder.buildSessionFactory();
+}
+
+@Autowired
+@Bean(name = "transactionManager")
+public HibernateTransactionManager getTransactionManager(
+SessionFactory sessionFactory) {
+HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+sessionFactory);
+
+return transactionManager;
+}
+}
+
---
+++ urwardrobebackend/src/main/java/com/urwardrobe/model/User.java
@@ -0,0 +1,59 @@
+package com.urwardrobe.model;
+import javax.persistence.Column; 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+ 
+@Entity
+@Table(name = "USERS")
+public class User {
+
+	 
+	    private String firstname;
+	    private String lastname;
+	    private String password;
+	    private String email;
+	    private String confirm_password;
+	    
+	    @Id
+	    @GeneratedValue
+	    @Column(name = "")
+	    
+	    
+		
+		public String getFirstname() {
+			return firstname;
+		}
+		public void setFirstname(String firstname) {
+			this.firstname = firstname;
+		}
+		public String getLastname() {
+			return lastname;
+		}
+		public void setLastname(String lastname) {
+			this.lastname = lastname;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getConfirm_password() {
+			return confirm_password;
+		}
+		public void setConfirm_password(String confirm_password) {
+			this.confirm_password = confirm_password;
+		}
+
+	 
+
+}
